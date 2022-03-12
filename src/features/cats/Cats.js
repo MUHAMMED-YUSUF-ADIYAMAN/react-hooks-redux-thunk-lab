1cf81c9829d98ae7cdf2c7c7dc9d116da862a9ef
myusufadiyaman <49249673+MUHAMMED-YUSUF-ADIYAMAN@users.noreply.github.com>
2022-03-12 21:43:46 +0300

    .

--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,12 +1,24 @@
-import React from "react";
-
+// src/features/cats/Cats.js
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCats } from "./catsSlice";
+import CatList from "./CatList"
 function Cats() {
+  const catPics = useSelector((state) => state.entities);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCats());
+  }, []);
+
+  console.log(catPics);
   return (
-    <div>
+    <div className="App">
       <h1>CatBook</h1>
-      {/* add CatList component here */}
+      <CatList catPics={catPics} />
+      {/* missing component */}
     </div>
   );
 }
 
-export default Cats;
+export default Cats; 
